d4bea87df092b3d98fec337ec066cc7c32829384
Musa-Ismailov <164714080+Musa-Ismailov@users.noreply.github.com>
2025-04-19 01:14:14 +0000

    Deploying to gh-pages from @ Musa-Ismailov/2252139-cat-energy-2@a6bb8f374a85055a20edd9a76ca54935f3bea6e7 🚀

--- 11/scripts/index.js
+++
@@ -1,1 +0,0 @@
-var n=document.querySelector(".main-nav"),e=document.querySelector(".main-nav__toggle");e.addEventListener("click",()=>{n.classList.toggle("main-nav--on"),n.classList.toggle("main-nav--off")});
